68360c1ecdd19c5e9a842c8b59fe152bbdd279c1
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2025-05-06 12:33:01 -0600

    use zod instead

--- src/schema.test.ts
+++ src/schema.test.ts
@@ -2,8 +2,8 @@ import { schema } from './schema'
 const integrationList = require('./integration-list.json')
 
 describe('integration-list.json', () => {
-  it('complies with schema', () => {
-    const validationResult = schema.validate(integrationList)
-    expect(validationResult.error).toBe(undefined)
+  it('complies with schema', async () => {
+    const result = await schema.safeParse(integrationList)
+    expect(result.success).toBe(true)
   })
 })
--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,12 @@
-import Joi from 'joi'
+import { z } from 'zod'
 
 const walletAddressPattern = /^0x[a-fA-F0-9]{40}$/
 
-export const schema = Joi.array().items(
-  Joi.object({
-    entityAddress: Joi.string().pattern(walletAddressPattern).required(),
-    githubUsername: Joi.string().required(),
+export const schema = z.array(
+  z.object({
+    entityAddress: z
+      .string()
+      .regex(walletAddressPattern, 'Invalid wallet address'),
+    githubUsername: z.string().min(1, 'GitHub username is required'),
   }),
 )
