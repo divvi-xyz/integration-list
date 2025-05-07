180825f3db72712d4991dc1f6f9c6739e4c254ac
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2025-05-07 10:52:08 -0600

    feedback

--- src/schema.ts
+++ src/schema.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod'
 
-const walletAddressPattern = /^0x[a-fA-F0-9]{40}$/
+const walletAddressPattern = /^0x[a-f0-9]{40}$/
 
 export const schema = z.array(
   z.object({
     entityAddress: z
       .string()
-      .regex(walletAddressPattern, 'Invalid wallet address'),
-    githubUsername: z.string().min(1, 'GitHub username is required'),
+      .regex(walletAddressPattern, 'Invalid wallet address. We only accept lowercased addresses.'),
+    githubUsername: z.string().min(1, 'GitHub username is required.'),
   }),
 )
